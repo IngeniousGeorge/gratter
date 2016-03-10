03dc2f7541ec926943ff7089984349d889ebaef0
JC <jcdrytherain@yahoo.com>
2016-03-10 14:26:37 +0100

    disaster with xpath

--- lib/gratter_class.rb
+++ lib/gratter_class.rb
@@ -2,17 +2,25 @@ require 'nokogiri'
 require 'net/http'
 
 class Gratter
-  def initialize url
+
+  attr_reader :url
+  attr_reader :xpath
+
+  def initialize(url, xpath)
     @url = url
+    @xpath = xpath
   end
 
-  def url
-    @url
-  end
-
-  def use
+  def parse
     parser = Parser.new @url
     parser.parse
+  end
+
+  def xpath
+    parser = Parser.new @url
+    doc = parser.parse
+    xpather = Xpather.new @xpath
+    xpather.xpath(doc xpath)
   end
 
 end
@@ -28,6 +36,19 @@ class Parser
     body = Net::HTTP.get(uri)
     document = Nokogiri::HTML(body)
     return document
+  end
+
+end
+
+class Xpather
+
+  def initialize(doc, xpath)
+    @doc = doc
+    @xpath = xpath
+  end
+
+  def xpath
+    "/"
   end
 
 end
--- spec/gratter_class_spec.rb
+++ spec/gratter_class_spec.rb
@@ -4,15 +4,27 @@ require_relative "../lib/gratter_class.rb"
 describe "gratter" do
 
   let(:url) { "http://www.livefootball.com" }
-  let(:inst) { Gratter.new "http://www.livefootball.com" }
+  let(:inst) { Gratter.new "http://www.livefootball.com" "//a[@class='btn btnHome']/@href" }
+  let(:doc) { inst.parse }
 
   it "has a URL" do
-    raise unless Gratter.new("http://www.livefootball.com").url == url
+    expect(inst.url).to eq("http://www.livefootball.com")
   end
 
-  it "returns a Nokogiri document" do
-    doc = inst.use
-    expect(doc.class).to eql Nokogiri::HTML::Document
+  describe "parser" do
+
+    it "returns a Nokogiri document" do
+      expect(doc.class).to eql Nokogiri::HTML::Document
+    end
+
+  end
+
+  describe "xpather" do
+
+    it "returns a part of a document " do
+      expect(doc.xpath("//a[@class='btn btnHome']/@href")).to eq("/")
+    end
+
   end
 
 end
