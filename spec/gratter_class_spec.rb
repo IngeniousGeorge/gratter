e308f29053e9f91a3495dadc63176972ee04838a
JC <jcdrytherain@yahoo.com>
2016-03-11 14:55:11 +0100

    "Removed extra data (team name+pts) in test"

--- spec/gratter_class_spec.rb
+++ spec/gratter_class_spec.rb
@@ -4,16 +4,16 @@ require_relative "../lib/gratter_class.rb"
 describe "gratter" do
 
   let(:url) { "http://www.livefootball.com/football/england/premier-league/league-table/" }
-  let(:xpaths) { { :team => "//td[@class='ltn']/text()", :points => "//td[@class='ltp']/text()" } }
+  let(:xpaths) { { :team => "(//td[@class='ltn'])[position()>1]/text()", :points => "(//td[@class='ltp'])[position()>1]/text()" } }
   let(:instance) { Gratter.new( { :url => url, :xpaths => xpaths } ) }
-  let(:results) { {:team=>["Team Name", "Leicester City", "Tottenham Hotspur", "Arsenal", "Manchester City", "West Ham United", "Manchester United", "Liverpool", "Stoke City", "Southampton", "Chelsea", "West Bromwich Albion", "Everton", "Watford", "AFC Bournemouth", "Crystal Palace", "Swansea City", "Sunderland", "Norwich City", "Newcastle United", "Aston Villa"], :points=>["PTS", "60", "55", "52", "50", "49", "47", "44", "43", "41", "40", "39", "38", "37", "35", "33", "33", "25", "24", "24", "16"]} }
+  let(:results) { {:team=>["Leicester City", "Tottenham Hotspur", "Arsenal", "Manchester City", "West Ham United", "Manchester United", "Liverpool", "Stoke City", "Southampton", "Chelsea", "West Bromwich Albion", "Everton", "Watford", "AFC Bournemouth", "Crystal Palace", "Swansea City", "Sunderland", "Norwich City", "Newcastle United", "Aston Villa"], :points=>["60", "55", "52", "50", "49", "47", "44", "43", "41", "40", "39", "38", "37", "35", "33", "33", "25", "24", "24", "16"]} }
 
   it "has a URL" do
     expect(instance.url).to eq("http://www.livefootball.com/football/england/premier-league/league-table/")
   end
 
   it "has tags and xpaths" do
-    expect(instance.xpaths).to eq( { :team => "//td[@class='ltn']/text()", :points => "//td[@class='ltp']/text()" } )
+    expect(instance.xpaths).to eq( { :team => "(//td[@class='ltn'])[position()>1]/text()", :points => "(//td[@class='ltp'])[position()>1]/text()" } )
   end
 
   it "returns the selected nodes from the document as strings in a hash when use is called" do
