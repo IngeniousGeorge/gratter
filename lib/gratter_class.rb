418eb441aeca48876bfa69f942027d68e90609d2
JC <jcdrytherain@yahoo.com>
2016-03-16 17:25:52 +0100

    Matcher completely refactored

--- lib/gratter_class.rb
+++ lib/gratter_class.rb
@@ -65,132 +65,44 @@ class Matcher
   end
 
   def match
-    arrays_sizes = get_arrays_sizes
-    check_data(arrays_sizes)
-    reorder_data(arrays_sizes) if arrays_sizes.max[1] != arrays_sizes.min[1]
-    match_nodes(arrays_sizes.max)
+    sizes = get_arrays_sizes
+    check_for_uniform_arrays(sizes)
+    make_arrays_uniform(sizes) if sizes.max[1] != sizes.min[1]
+    match_nodes(sizes.max[1])
   end
 
-  def get_arrays_sizes
-    sizes = {}
-    data.each { |tag,value| sizes[tag] = value.size }
-    puts sizes
-    return sizes #puts sizes #=> {:album=>1, :artist=>1, :rating=>1} / {:team=>21, :points=>21, :league=>1}
-  end
+    def get_arrays_sizes
+      sizes = {}
+      data.each { |tag,value| sizes[tag] = value.size }
+      return sizes #puts sizes #=> {:album=>1, :artist=>1, :rating=>1} / {:team=>21, :points=>21, :league=>1}
+    end
 
-  def check_data sizes
-    uniques = sizes.values.uniq # p uniques => [1] / [21, 1]
-    uniques.delete(1) if uniques.include?(1) && uniques.size > 1
-    raise "problem" if uniques.size > 1
-  end
+    def check_for_uniform_arrays sizes
+      uniques = sizes.values.uniq # p uniques => [1] / [21, 1]
+      uniques.delete(1) if uniques.include?(1) && uniques.size > 1
+      raise "problem" if uniques.size > 1
+    end
 
-  def reorder_data sizes
-    puts "here"
-    max = sizes.max[1]
-    puts max
-    data.each do |tag,value|
-      if value.size == 1 then
-        puts "found"
-        array = []
-        array = max.times { array << value }
-        puts array
-        data[tag] = array
+    def make_arrays_uniform sizes
+      max = sizes.max[1]
+      data.each do |tag,value|
+        if value.size == 1 then
+          array = []
+          max.times { array << value[0] }
+          data[tag] = array
+        end
       end
     end
-  end
 
-  def match_nodes size
-    result = Array.new(size) { {} }
-    data.each do |tag, array|
-      array.each_with_index do |value,index|
-        result[index][tag] = value
+    def match_nodes size
+      result = Array.new(size) { {} }
+      data.each do |tag, array|
+        array.each_with_index do |value,index|
+          hash = { tag => value }
+          result.at(index).merge!( hash ) #= [tag] = value # p tag, array, value, index => :album / ["No One Deserves Happiness"] / "No One Deserves Happiness" / 0
+        end
       end
+      return result
     end
-    return result
-  end
 
 end
-
-  # def arrays_differ_in_size?
-  #   sizes = []
-  #   data.each do |tag,value|
-  #     sizes << value.size unless value.class != Array
-  #   end
-  #   return false if sizes.empty?
-  #   sizes.uniq.size != 1
-  # end
-  #
-  # def single_nodes_only?
-  #   single_nodes = 0
-  #   data.each do |tag,value|
-  #     single_nodes += 1 if value.class == String
-  #   end
-  #   single_nodes == data.size
-  # end
-  #
-  # def strings_mixed_with_arrays?
-  #   single_nodes = 0
-  #   arrays = 0
-  #   data.each do |tag,value|
-  #     single_nodes += 1 if value.class == String
-  #     arrays += 1 if value.class == Array
-  #   end
-  #   single_nodes > 0 && arrays > 0
-  # end
-  #
-  # # def reorder_data
-  # #   data.each do |tag,value|
-  # #     if value.class == String then
-  # #       data[tag] = Array.new(3) { value.to_s }
-  # #     end
-  # #   end
-  # #   return data
-  # # end
-  #
-  # def get_max_num_of_nodes
-  #   sizes = []
-  #   data.each do |tag,value|
-  #     sizes << value.size unless value.class != Array
-  #   end
-  #   sizes.max
-  # end
-  #
-  # def get_min_num_of_nodes
-  #   sizes = []
-  #   data.each do |tag,value|
-  #     sizes << value.size unless value.class != Array
-  #   end
-  #   sizes.max
-  # end
-  #
-  # def all_arrays_same_size?
-  #   sizes = []
-  #   data.each do |tag,value|
-  #     sizes << value.size unless value.class != Array
-  #   end
-  #   sizes.uniq.size == 1
-  # end
-  #
-  # def create_full_arrays_for_single_nodes
-  #   data.each do |tag,value|
-  #     if value.class == String then
-  #       #data[tag] = Array.new(@max_num_of_nodes) { value.to_s }
-  #     end
-  #   end
-  #   return data
-  # end
-  #
-  #
-  # def match_single_nodes
-  #   data.each do |tag, val|
-  #     data[tag] = val.to_s
-  #   end
-  #   return [data]
-  # end
-
-
-
-# raise "Invalid data: Node lists are not of the same size and cannot be matched" if arrays_differ_in_size?
-# return match_single_nodes if single_nodes_only?
-# @data = reorder_data if strings_mixed_with_arrays?
-# return match_many_nodes
