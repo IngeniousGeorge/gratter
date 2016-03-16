8ca2cb6df1760aaa912cf2a2ee29269f930ca551
JC <jcdrytherain@yahoo.com>
2016-03-16 10:05:22 +0100

    Done refactoring T for Gratter, Parser & Xpather

--- spec/gratter_class_spec.rb
+++ spec/gratter_class_spec.rb
@@ -5,6 +5,7 @@ describe "gratter" do
 
   let(:url) { "http://www.livefootball.com/football/england/premier-league/league-table/" }
   let(:xpaths) { { :team => "(//td[@class='ltn'])[position()>1]/text()", :points => "(//td[@class='ltp'])[position()>1]/text()" } }
+  let(:expected_max_num_of_nodes) { 20 }
   let(:instance) { Gratter.new( { :url => url, :xpaths => xpaths } ) }
   let(:data) { instance.use }
 
@@ -20,11 +21,15 @@ describe "gratter" do
     expect(data.class).to be(Array)
   end
 
+  it "returns an array of size equal to the max number of nodes" do
+    expect(data.size).to eql(expected_max_num_of_nodes)
+  end
+
   describe "Single Classes" do
 
     describe "Parser" do
 
-      let(:parser) { Parser.new("http://www.google.com") }
+      let(:parser) { Parser.new("http://www.duckduckgo.com") }
 
       it "returns a Nokogiri document on parse" do
         expect(parser.parse.class).to eql(Nokogiri::HTML::Document)
@@ -34,20 +39,19 @@ describe "gratter" do
 
     describe "Xpather" do
 
-      let(:xpather) { Xpather.new(Parser.new("http://www.livefootball.com/football/england/premier-league/league-table/").parse, xpaths) }
+      let(:xpaths) { { :tag1 => "//div", :tag2 => "//span" } }
+      let(:xpather) { Xpather.new(Parser.new("http://www.duckduckgo.com").parse, xpaths) }
+      let(:data) { xpather.get_data }
 
       it "has a Nokogiri document to work with" do
-        expect(xpather.doc.class).to eql Nokogiri::HTML::Document
+        expect(xpather.doc.class).to eql(Nokogiri::HTML::Document)
       end
 
-      it "returns a hash with the tags and the selected data on get_data" do
-        data = instance.use
-        expect(data[:team].class).to be(Array)
-        expect(data[:team].include?("Leicester City")).to be_truthy
-        expect(data[:team].include?("Manchester United")).to be_truthy
-        expect(data[:points][10]).to match(/\d{2}/)
-        expect(data[:points][0]).to be >= data[:points][19]
+      it "returns a hash of the same size as the xpaths" do
+        expect(data.class).to eql(Hash)
+        expect(data.size).to eql(xpaths.size)
       end
+
     end
 
     describe "Matcher" do
