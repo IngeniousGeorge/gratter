d39e0e499bc24d6e935c78d33ec58e83db8fca8f
JC <jcdrytherain@yahoo.com>
2016-03-17 18:39:34 +0100

    Tidied up a bit

--- lib/gratter.rb
+++ lib/gratter.rb
@@ -1,20 +1,24 @@
 require_relative 'gratter_class'
+require 'date'
 
+# Livefootball
 url = "http://www.livefootball.com/football/england/premier-league/league-table/"
 xpaths = { :team => "(//td[@class='ltn'])[position()>1]/text()", :points => "(//td[@class='ltp'])[position()>1]/text()", :league => "//div[@class='header']/h2/text()" }
-trans_pattern = { :league => Proc.new { |node| node.gsub /.*&#8211; /, '' } }
+to_be_added = { :date => "" }
+trans_pattern = { :league => Proc.new { |node| node.gsub /.*&#8211; /, '' }, :date => Proc.new { |node| (Date.today << 1).to_s } }
+
+# Livescore
 # url = "http://www.livescore.com/soccer/england/premier-league/"
 # xpaths = { :team => "//div[@class='team']/text()", :points => "//div[@class='pts tot']/text()", :league => "//div[@class='left']//a//text()" }
+
+# Pitchfork
 # url = "http://pitchfork.com/reviews/albums/21631-no-one-deserves-happiness/"
 # xpaths = { :album => "//h1[@class='review-title']/text()", :artist => "//h2[@class='artists']//a/text()", :rating => "//span[@class='score']/text()" }
 
-football = Gratter.new( { url: url, xpaths: xpaths, trans_pattern: trans_pattern } )
 
-puts football.use
+result = Gratter.new( { url: url, xpaths: xpaths, to_be_added: to_be_added, trans_pattern: trans_pattern } )
 
-s = "England &#8211; Premier League"
-s.slice! "England &#8211; "
-p s
+puts result.use
 
 # parser = Parser.new(url)
 #
